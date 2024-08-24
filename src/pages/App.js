c6eeb7f03446cab7ab76e70576d4c3b1345132fa
jsyoon415 <jslab415@naver.com>
2024-08-24 09:52:53 +0900

    add bot description

--- src/pages/App.js
+++ src/pages/App.js
@@ -6,6 +6,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { Link } from 'react-router-dom';
 
+import img1 from '../assets/img1.png';
+import img2 from '../assets/img2.png';
+
 function App() {
     return (
         <div className="App">
@@ -16,11 +19,12 @@ function App() {
                     디스코드 악성 유저 차단 봇, <br />
                     MGuard.
                 </h1>
+
                 <p className={css.description}>
                     악성 유저 차단 외 다양한 보안 기능들 포함!
                     <br />
                     <br />
-                    <span style={{ fontFamily: 'Pretendard-ExtraBold' }}>99999</span>개의 서버에서 일하는 중 🐜
+                    <span style={{ fontFamily: 'Pretendard-ExtraBold' }}>n</span>개의 서버에서 일하는 중 🐜
                     <br />
                     <br />
                 </p>
@@ -28,8 +32,32 @@ function App() {
                     <button className="btn btn-green btn-large">봇 초대하기</button>
                 </Link>
                 <Link to={''} target="_blank" rel="noreferrer noopener">
-                    <button className="btn btn-blurple btn-large">웹 대시보드 바로가기</button>
+                    <button className="btn btn-blurple btn-large">서포트 서버 바로가기</button>
                 </Link>
+            </div>
+            <hr />
+            <div className="centerMargin">
+                <h1>방대한 악성 유저 DB와 api</h1>
+                <img src={img1} style={{ width: '45%' }} alt="img1" />
+                <p>
+                    Mguard로 자신의 서버를 악성 유저들로 부터 보호하세요! 악성 유저 데이터가 필요하신 개발자들을 위해
+                    api도 무료로 제공하고 있어요.
+                </p>
+                <h1>유저 관리 시스템</h1>
+                <img src={img2} style={{ width: '45%' }} alt="img2" />
+                <p>욕설 / 스팸 검열 기능, 경고 기능, 차단, 추방 기능, 신고 기능 등이 봇에 내장되어 있어요!</p>
+                <br />
+                <br />
+                <br />
+                <div style={{ textAlign: 'center' }}>
+                    <h1>MGuard로 당신의 서버를 악성 유저로부터 보호하세요!</h1>
+                    <Link to={''} target="_blank" rel="noreferrer noopener">
+                        <button className="btn btn-green btn-large">봇 초대하기</button>
+                    </Link>
+                    <Link to={''} target="_blank" rel="noreferrer noopener">
+                        <button className="btn btn-blurple btn-large">서포트 서버 바로가기</button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
